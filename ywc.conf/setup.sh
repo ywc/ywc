fde074b2fb4a4699b406ae1a806f74f488531a79
topherwhite <topherwhite@gmail.com>
2013-01-14 02:11:29 -0800

    set script (currently semi tailored to oist)

--- ywc.conf/setup.sh
+++ ywc.conf/setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+export APP_NAME=oist
+export SVN_BASE=https://yeswecan-oist.googlecode.com/svn/trunk
+
 export YWC_HOME=/data/ywc
+
 mkdir $YWC_HOME/ywc.cache
 mkdir $YWC_HOME/ywc.cache/doc $YWC_HOME/ywc.cache/tmp $YWC_HOME/ywc.cache/txt $YWC_HOME/ywc.cache/xml
 mkdir $YWC_HOME/ywc.cache/doc/orig $YWC_HOME/ywc.cache/doc/temp $YWC_HOME/ywc.cache/doc/thmb $YWC_HOME/ywc.cache/doc/work
@@ -14,3 +18,7 @@ cp $YWC_HOME/ywc.conf/data.xml $YWC_HOME/ywc.cache/xml/data/transform.xml
 cp $YWC_HOME/ywc.conf/data.xml $YWC_HOME/ywc.cache/xml/data/uri.xml
 cp $YWC_HOME/ywc.conf/data.xml $YWC_HOME/ywc.cache/xml/data/user.xml
 chmod -Rf a+rw $YWC_HOME/ywc.cache
+
+svn checkout $SVN_BASE/ywc.db/oist $YWC_HOME/ywc.db/oist
+svn checkout $SVN_BASE/ywc.lib/oist $YWC_HOME/ywc.lib/oist
+svn checkout $SVN_BASE/ywc.xsl/oist $YWC_HOME/ywc.xsl/oist
