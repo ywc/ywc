e3a75263d57dc3a6d5bddf58c01e58611226ab9c
topherwhite <topherwhite@gmail.com>
2013-02-04 23:04:51 -0800

    authorname changes

---
+++ ywc.conf/update-repos.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# navigate to ywc home
+cd /data/ywc/;
+
+# set app name
+export YWC_APP=oist;
+
+# update lib assets for app
+cd ywc.lib/$YWC_APP/;
+svn cleanup;
+svn up;
+
+# update xsl assets for app
+cd ../../ywc.xsl/$YWC_APP/;
+svn cleanup;
+svn up;
+
+# update database for app
+cd ../../ywc.db/$YWC_APP/;
+svn cleanup;
+svn up;
+mysql -hlocalhost -uywc -pywcywc < ywcoist.sql;
+
+# update ywc core
+cd ../../;
+git pull;
