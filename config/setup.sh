6a0d69818cd693250a7d584da72b49ff4ce69ddf
topherwhite <topherwhite@gmail.com>
2013-09-22 16:44:11 -0700

    data-default rename

--- config/setup.sh
+++ config/setup.sh
@@ -7,14 +7,14 @@ mkdir $YWC_HOME/cache/doc $YWC_HOME/cache/tmp $YWC_HOME/cache/txt $YWC_HOME/cach
 mkdir $YWC_HOME/cache/doc/orig $YWC_HOME/cache/doc/temp $YWC_HOME/cache/doc/thmb $YWC_HOME/cache/doc/work
 mkdir $YWC_HOME/cache/tmp/gen $YWC_HOME/cache/tmp/img $YWC_HOME/cache/tmp/ing $YWC_HOME/cache/tmp/txt $YWC_HOME/cache/tmp/upl $YWC_HOME/cache/tmp/xml
 mkdir $YWC_HOME/cache/xml/cache $YWC_HOME/cache/xml/data
-cp $YWC_HOME/config/data.xml $YWC_HOME/cache/xml/data/cache.xml
-cp $YWC_HOME/config/data.xml $YWC_HOME/cache/xml/data/include.xml
-cp $YWC_HOME/config/data.xml $YWC_HOME/cache/xml/data/placeholder.xml
-cp $YWC_HOME/config/data.xml $YWC_HOME/cache/xml/data/scope.xml
-cp $YWC_HOME/config/data.xml $YWC_HOME/cache/xml/data/template.xml
-cp $YWC_HOME/config/data.xml $YWC_HOME/cache/xml/data/transform.xml
-cp $YWC_HOME/config/data.xml $YWC_HOME/cache/xml/data/uri.xml
-cp $YWC_HOME/config/data.xml $YWC_HOME/cache/xml/data/user.xml
+cp $YWC_HOME/config/data-default.xml $YWC_HOME/cache/xml/data/cache.xml
+cp $YWC_HOME/config/data-default.xml $YWC_HOME/cache/xml/data/include.xml
+cp $YWC_HOME/config/data-default.xml $YWC_HOME/cache/xml/data/placeholder.xml
+cp $YWC_HOME/config/data-default.xml $YWC_HOME/cache/xml/data/scope.xml
+cp $YWC_HOME/config/data-default.xml $YWC_HOME/cache/xml/data/template.xml
+cp $YWC_HOME/config/data-default.xml $YWC_HOME/cache/xml/data/transform.xml
+cp $YWC_HOME/config/data-default.xml $YWC_HOME/cache/xml/data/uri.xml
+cp $YWC_HOME/config/data-default.xml $YWC_HOME/cache/xml/data/user.xml
 chmod -Rf a+rw $YWC_HOME/cache;
 
 # cd $YWC_HOME/config/;
