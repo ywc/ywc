5dbf7377ff1a047b341fff86c61808748af626a5
topherwhite <topherwhite@gmail.com>
2013-10-02 13:01:16 -0700

    install scripts

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ ywc
 ===
 
 A multi-platform web development framework based on server-side XSLT and client-side Javascript.
+
+Install with this line;
+
+`curl -s -L https://raw.github.com/ywc/ywc/master/config/ywc/install.sh -o install-ywc.sh; chmod a+x install-ywc.sh; ./install-ywc.sh;`
--- config/ywc/install.sh
+++ config/ywc/install.sh
@@ -11,7 +11,11 @@ if [ ! -d cache ]; then
   mkdir cache cache/doc cache/tmp cache/txt cache/xml cache/doc/orig cache/doc/temp cache/doc/thmb cache/doc/work cache/tmp/gen cache/tmp/img cache/tmp/ing cache/tmp/txt cache/tmp/upl cache/tmp/xml cache/xml/cache cache/xml/data;
   cat config/ywc/templates/data-default.xml | tee cache/xml/data/cache.xml cache/xml/data/include.xml cache/xml/data/placeholder.xml cache/xml/data/scope.xml cache/xml/data/template.xml cache/xml/data/transform.xml cache/xml/data/uri.xml cache/xml/data/user.xml > /dev/null;
 fi
-chmod -Rf a+rw cache ywc.core/ywc.core/xml/data;
+chmod -Rf 1777 cache ywc.core/ywc.core/xml/data;
+
+yum install -y tomcat6 tomcat6-webapps tomcat6-admin-webapps ImageMagick;
 
 # add node to server.xml so that tomcat also runs on 8081
 #vi ~tomcat/conf/server.xml
+
+# curl -s -L https://raw.github.com/ywc/ywc/master/config/ywc/install.sh -o install-ywc.sh; chmod a+x install-ywc.sh; ./install-ywc.sh;
