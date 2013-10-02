1cb353ff592c5505ed38eb6c4c9ec51e351c2b43
topherwhite <topherwhite@gmail.com>
2013-10-02 04:36:02 -0700

    consolidated update script

--- config/ywc/update.sh
+++ config/ywc/update.sh
@@ -7,8 +7,14 @@ cd $YWC_HOME;
 
 export YWC_APP=`cat config/app_name`;
 
+# pulling in latest core
 git pull;
 
-config/ywc/database-rebuild.sh
+# rebuilding SQLite databases
+rm database/ywc/ywccore.sqlite3 database/$YWC_APP/ywc$YWC_APP.sqlite3;
+sqlite3 database/ywc/ywccore.sqlite3 < database/ywc/ywccore.sqlite.sql;
+sqlite3 database/$YWC_APP/ywc$YWC_APP.sqlite3 < database/$YWC_APP/ywc$YWC_APP.sqlite.sql;
+chmod a+rwx database/ywc/ywccore.sqlite3 database/$YWC_APP/ywc$YWC_APP.sqlite3;
 
-config/ywc/cache-rebuild.sh
+# running backend.d
+java -jar ywc.core/ywc.java/ywc.backend.d/dist/ywc.backend.d.jar;
