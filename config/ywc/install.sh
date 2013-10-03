ffeea1e2ed18c46403a5796382be27acd2c3ca45
topherwhite <topherwhite@gmail.com>
2013-10-02 18:53:46 -0700

    installation and publication scripts

--- config/ywc/install.sh
+++ config/ywc/install.sh
@@ -11,18 +11,13 @@ echo "Enter the YWC app 'name': ";
 read APP_NAME;
 echo "Your app will be named '$APP_NAME'.";
 echo $APP_NAME > config/app_name;
+echo "UNFIXED BUG!!!!! PLEASE REMOVE TRAILING CARRIAGE RETURN IN APP_NAME FILE!!!";
 
 if [ ! -d cache ]; then
   mkdir cache cache/doc cache/tmp cache/txt cache/xml cache/doc/orig cache/doc/temp cache/doc/thmb cache/doc/work cache/tmp/gen cache/tmp/img cache/tmp/ing cache/tmp/txt cache/tmp/upl cache/tmp/xml cache/xml/cache cache/xml/data;
   cat config/ywc/templates/data-default.xml | tee cache/xml/data/cache.xml cache/xml/data/include.xml cache/xml/data/placeholder.xml cache/xml/data/scope.xml cache/xml/data/template.xml cache/xml/data/transform.xml cache/xml/data/uri.xml cache/xml/data/user.xml > /dev/null;
 fi
 chmod -Rf 1777 cache ywc.core/ywc.core/xml/data;
-
-echo "Enter the YWC app 'name': ";
-read APP_NAME;
-echo "Your app will be named '$APP_NAME'.";
-echo $APP_NAME > config/app_name;
-echo "UNFIXED BUG!!!!! PLEASE REMOVE TRAILING CARRIAGE RETURN IN APP_NAME FILE!!!";
 
 yum install -y tomcat6 tomcat6-webapps tomcat6-admin-webapps ImageMagick;
 
