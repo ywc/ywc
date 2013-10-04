b01a85fb13c707ab7a59fa91839a45df1d559c6c
topherwhite <topherwhite@gmail.com>
2013-10-03 20:12:39 -0700

    cache as single script

---
+++ config/ywc/cache.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+cd $SCRIPT_DIR/../..;
+export YWC_HOME=`pwd`;
+
+# running backend.d
+java -jar ywc.core/ywc.java/ywc.backend.d/dist/ywc.backend.d.jar;
--- config/ywc/update.sh
+++ config/ywc/update.sh
@@ -3,7 +3,6 @@
 export SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
 cd $SCRIPT_DIR/../..;
 export YWC_HOME=`pwd`;
-cd $YWC_HOME;
 
 export YWC_APP=`cat config/app_name`;
 
@@ -16,6 +15,5 @@ sqlite3 database/ywc/ywccore.sqlite3 < database/ywc/ywccore.sqlite.sql;
 sqlite3 database/$YWC_APP/ywc$YWC_APP.sqlite3 < database/$YWC_APP/ywc$YWC_APP.sqlite.sql;
 chmod a+rwx database/ywc/ywccore.sqlite3 database/$YWC_APP/ywc$YWC_APP.sqlite3;
 
-# running backend.d
-java -jar ywc.core/ywc.java/ywc.backend.d/dist/ywc.backend.d.jar;
+config/ywc/cache.sh;
 
