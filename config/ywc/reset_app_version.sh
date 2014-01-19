34cc942ae8f7a804f9e94bf117cf12c43890a8af
topherwhite <topherwhite@gmail.com>
2014-01-18 18:10:33 -0800

    app version generation script

---
+++ config/ywc/reset_app_version.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+cd $SCRIPT_DIR/../..;
+
+export RAND=$RANDOM;
+for (( b=0; b<32000; b++ ))
+do
+  export RAND=$(( RAND+$RANDOM ));
+done
+
+# turns random number into 10 character hash
+export APP_VERSION=`echo -n $RAND | md5 | cut -c 1-10`;
+
+# saves hash to local file
+echo $APP_VERSION > config/app_version;
